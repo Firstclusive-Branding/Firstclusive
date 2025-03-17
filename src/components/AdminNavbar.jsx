c050b88b7b7a0f0f84a6f5a867ce28b82f70d944
Firstclusive-Branding <firstclusiveoffice@gmail.com>
2025-03-17 15:28:43 +0530

    update

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/adminNavbar.css";
+import "../styles/AdminNavbar.css";
 
 const AdminNavbar = () => {
   const navigate = useNavigate();
